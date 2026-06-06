bbe36a7fa7e7faa912d2b2ef6d3dacba124915da
Pa1Kasyap <pavankasyap91@gmail.com>
2026-06-06 23:23:49 +0530

    Factorial code

---
+++ FactorialCode/Factorial/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Factorial
+{
+    public void Multiply(int n)
+    {
+        long factorial = 1;
+        if(n > 0)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine("Factorial of "+n+" is "+factorial);
+        }
+        else
+        {
+            Console.WriteLine("Factorial is not possible for this number");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number: ");
+        int num = int.Parse(Console.ReadLine());
+        Factorial factorial = new Factorial();
+        factorial.Multiply(num);
+    }
+}
